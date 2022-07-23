c4bf3fa05fd85b97b65b3bc522505ab2fb802559
Joshua Miller <jmyoshibanks@gmail.com>
2022-07-23 14:23:29 -0700

    added signout button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,76 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import "@aws-amplify/ui-react/styles.css";
 import './App.css';
-import { withAuthenticator, AmplifySignOut, AmplfiyProvider } from '@aws-amplify/ui-react'
+import { 
+  withAuthenticator, 
+  AmplfiyProvider,
+  Authenticator,
+  Button,
+} from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
+import { SideBar } from './ui-components/SideBar';
+import '@aws-amplify/ui-react/styles.css';
 
 import awsconfig from './aws-exports';
 import aws_exports from './aws-exports';
+import awsExports from './aws-exports';
 
+Amplify.configure(awsExports);
 Amplify.configure(awsconfig);
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. V2 Hello World
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Authenticator initialState='signUp'>
+        {({ signOut, user }) => (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload. V2 Hello {user.username}.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+              <Button
+              LoadingText=""
+              onClick={signOut}
+              >
+                Sign out
+              </Button>
+            </header>
+          </div>
+        )}
+      </Authenticator>
     );
   }
 }
 
+function TestApp() {
+  return (
+    <Authenticator initialState="signUp">
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
 export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
